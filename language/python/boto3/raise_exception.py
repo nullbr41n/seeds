1400011959f4db54ee0405e8109bab690958596e
nullbrain <76875710+nullbr41n@users.noreply.github.com>
2021-11-13 23:39:01 +0000

    Update raise_exception.py

--- language/python/boto3/raise_exception.py
+++ language/python/boto3/raise_exception.py
@@ -4,6 +4,18 @@
 
 # Handy when you want to raise exception to test something like 
 # Retry mechanism such as https://pypi.org/project/backoff/
-raise ClientError({"Error": {"Code": "TooManyRequestsException",
-                             "Message": "TooManyRequestsException"}},
-                  'invoke')
+@backoff.on_exception(backoff.expo, ClientError, max_time=3)
+def func():
+  try:
+  #........
+  #........
+  #........
+  #........  
+    raise ClientError({"Error": {"Code": "TooManyRequestsException",
+                                 "Message": "TooManyRequestsException"}},
+                      'invoke')
+    except ClientError as error:
+        if error.response['Error']['Code'] == 'TooManyRequestsException':
+            raise error
+        if error.response['Error']['Code'] == 'ResourceNotReadyException':
+            raise error
